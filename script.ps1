ef4b15fd4dbf3eb1ade08b865fba5d28e7536094
Yelodress <82002356+Yelodress@users.noreply.github.com>
2024-01-20 19:47:54 +0100

    v0.2.1

--- script.ps1
+++ script.ps1
@@ -3,13 +3,15 @@
 |  _ \ _____      _____ _ __ / \  _   _  __| (_) |_ 
 | |_) / _ \ \ /\ / / _ \ '__/ _ \| | | |/ _` | | __|
 |  __/ (_) \ V  V /  __/ | / ___ \ |_| | (_| | | |_ 
-|_|   \___/ \_/\_/ \___|_|/_/   \_\__,_|\__,_|_|\__|v0.2
+|_|   \___/ \_/\_/ \___|_|/_/   \_\__,_|\__,_|_|\__|v0.2.1
+
+
 "@
  
- Add-Type -AssemblyName PresentationFramework
 
 #-------------------------------------------- Warning messagebox ---------------------------------------------
 
+Add-Type -AssemblyName PresentationFramework # loa the assemply .NET framework (to make the script able to create a message box interface)
 $messageBoxText = "This script will collect some hardware and software information (such as your components, your disk space and your OS version).`nIf you don't want this, you can cancel the execution.`nExecute anyway ?"
 $caption = "Warning"
 $button = [System.Windows.MessageBoxButton]::OKCancel
@@ -69,7 +71,7 @@ $userName = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.Split
 $fileName = "results.csv"
 $fileName2 = "Applications-" + $userName + ".csv"
 
-#-------------------------------------------- Full installed softwale list ---------------------------------------------
+#-------------------------------------------- Full installed software list ---------------------------------------------
 
 $appsList = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*,
                                   HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
@@ -79,46 +81,36 @@ $appsList = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\Curren
 
 #---------------------------------------------- Defining objects -----------------------------------------------
 
-$totalFreeSpace = Get-Volume |  Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB'} # Get the total volume ingore USB, Removable and CD-ROM devices
-    $totalFreeSpace2 = ($totalFreeSpace | Measure-Object -Property SizeRemaining -Sum).Sum / 1GB # Measure the free space
-    $totalFreeSpaceGo = [math]::Round($totalFreeSpace2, 2) # Divide the free space (in MB) to get in in GB
+$diskInfo = Get-PhysicalDisk | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB'} #Get the disk type and his RPM (if it's HDD) 
+    $diskType = ($diskInfo | ForEach-Object { $_.MediaType}) -join ', ' # Put , separator between disks
+    $diskModel = ($diskInfo | ForEach-Object { $_.Model}) -join ', ' # Put , separator between disks
 
-$diskType = Get-PhysicalDisk | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB'}  | ForEach-Object {$_.MediaType} #Get the disk type and his RPM (if it's HDD) 
-    $diskType2 = $diskType -join ', '
+$systemInfo = Get-CimInstance Win32_ComputerSystem | Select-Object Manufacturer, Model, TotalPhysicalMemory, Domain # Obtain PC specs
 
-$disksModel= Get-PhysicalDisk | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB'}  | ForEach-Object {$_.Model} #Get the disk model
-    $disksModel2 = $disksModel -join ', '
-
-$systemInfo = Get-CimInstance Win32_ComputerSystem | Select-Object Manufacturer, Model, TotalPhysicalMemory # Obtain PC specs
-
-$systemMemory = [math]::Round($systemInfo.TotalPhysicalMemory / 1GB, 2)
-    $systemMemoryRounded = [math]::Ceiling($systemMemory)
-    $systemMemoryGB = "$systemMemoryRounded GB"
+$osInfo = Get-CimInstance Win32_OperatingSystem | Select-Object Caption, Version, OSArchitecture, CSName # Obtain OS informations
+    $systemMemory = [math]::Round($systemInfo.TotalPhysicalMemory / 1GB, 2)
+        $systemMemoryRounded = [math]::Ceiling($systemMemory)
+            $systemMemoryGB = "$systemMemoryRounded GB"
 
 $SN = Get-CimInstance Win32_BIOS | Select-Object SerialNumber # Obtain the computer S/N
- 
-$processorInfo = Get-CimInstance Win32_Processor | Select-Object Name # Obtain CPU model
-    $processorName = ($processorInfo | Select-Object -ExpandProperty Name)
 
-$cpuFrequency = Get-CimInstance Win32_Processor | Select-Object -ExpandProperty MaxClockSpeed
-    $cpuFrequencyGHz = $cpuFrequency / 1000 # Converting Hz to GHz
+$processorInfo = Get-CimInstance Win32_Processor | Select-Object Name, MaxClockSpeed # Obtain CPU name and max clock speed
  
 $gpuInfo = Get-CimInstance Win32_VideoController | ForEach-Object { $_.Name } # Obtain GPU model
     $gpuList = $gpuInfo -join ', '
 
 $currentDate = Get-Date -Format "yyyy-MM-dd" # Obtain the date
 
-$domainName = Get-CimInstance Win32_ComputerSystem | Select-Object Domain
 
 $encryptionStatus = manage-bde -status C: | Out-String #Check if your computer is encrypted by BitLocker
     $isEncrypted = if ($encryptionStatus -match "Protection On") { "Yes" } else { "No" } # Write "Yes" or "No"
 
-$osInfo = Get-CimInstance Win32_OperatingSystem | Select-Object Caption, Version, OSArchitecture, CSName
 
-$totalSpace = Get-Volume | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB'}
+$totalSpace = Get-Volume | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB'}  # Get the total volume ingore USB, Removable and CD-ROM devices
     $totalSpace2 = ($totalSpace | Measure-Object -Property Size -Sum).Sum / 1GB
-    $totalSpaceGo = [math]::Round($totalSpace2, 2)
-
+        $totalSpaceGo = [math]::Round($totalSpace2, 2)
+    $totalFreeSpace = ($totalSpace | Measure-Object -Property SizeRemaining -Sum).Sum / 1GB # Measure the free space
+        $totalFreeSpaceGo = [math]::Round($totalFreeSpace, 2) # Divide the free space (in MB) to get in in GB
 
 #----------------------------------------------- Finding office licence ------------------------------------------------
 
@@ -152,18 +144,18 @@ $combinedData = [PSCustomObject]@{
     "Manufacturer" = $systemInfo.Manufacturer
     "S/N" = $SN.SerialNumber
     "Computer name" = $osInfo.CSName
-    "CPU" = $processorName
-    "Frequency" = "$cpuFrequencyGHz GHz"
+    "CPU" = $processorInfo.Name
+    "Frequency" = ($processorInfo.MaxClockSpeed /1000).ToString() + " GHz"
     "GPU" = $gpuList
     "RAM" = $systemMemoryGB
     "Total disk space" = "$totalSpaceGo GB"
     "Total free space" = "$totalFreeSpaceGo GB"
-    "Disks type" = $diskType2
-    "Disks model" = $disksModel2
+    "Disks type" = $diskType
+    "Disks model" = $diskModel
     "OS" = $osInfo.Caption
     "Version" = $osInfo.Version
     "Architecture" = $osInfo.OSArchitecture
-    "Domain" = $domainName.Domain
+    "Domain" = $systemInfo.Domain
     "Office version" = $officeVersion
 #    "Specific software" = $softwareVersion
     "BitLocker encryption" = $isEncrypted
