947565fc205518f44183cc793763bbaf6ebfc477
Yelodress <82002356+Yelodress@users.noreply.github.com>
2024-02-15 23:05:43 +0100

    v0.4

--- script.ps1
+++ script.ps1
@@ -3,7 +3,7 @@
 |  _ \ _____      _____ _ __ / \  _   _  __| (_) |_ 
 | |_) / _ \ \ /\ / / _ \ '__/ _ \| | | |/ _` | | __|
 |  __/ (_) \ V  V /  __/ | / ___ \ |_| | (_| | | |_ 
-|_|   \___/ \_/\_/ \___|_|/_/   \_\__,_|\__,_|_|\__|v0.3
+|_|   \___/ \_/\_/ \___|_|/_/   \_\__,_|\__,_|_|\__|v0.4
 
 
 "@
@@ -11,19 +11,17 @@
 
 #-------------------------------------------- Warning messagebox ---------------------------------------------
 
-Add-Type -AssemblyName PresentationFramework # loa the assemply .NET framework (to make the script able to create a message box interface)
-$messageBoxText = "This script will collect some hardware and software information (such as your components, your disk space and your OS version).`nIf you don't want this, you can cancel the execution.`nExecute anyway ?"
+Add-Type -AssemblyName PresentationFramework # load the assemply .NET framework (to make the script able to create a message box interface)
 $caption = "Warning"
-$button = [System.Windows.MessageBoxButton]::OKCancel
+$messageBoxText = "This script will collect some hardware and software information (such as your components, your disk space and your OS version).`nIf you don't want this, you can cancel the execution.`nExecute anyway ?"
+$caption = [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::Default.GetBytes($caption))
+$text = [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::Default.GetBytes($messageBoxText))
 $icon = [System.Windows.MessageBoxImage]::Information
-$bytes = [System.Text.Encoding]::Default.GetBytes($messageBoxText)
-$textUtf8 = [System.Text.Encoding]::UTF8.GetString($bytes)
-$bytes = [System.Text.Encoding]::Default.GetBytes($caption)
-$captionUtf8 = [System.Text.Encoding]::UTF8.GetString($bytes)
+$button = [System.Windows.MessageBoxButton]::OKCancel
 
-$result = [System.Windows.MessageBox]::Show($textUtf8, $captionUtf8, $button, $icon) # Mixing all components
+$msgbox = [System.Windows.MessageBox]::Show($text, $caption, $button, $icon) # Mixing all components
 
-if ($result -eq "OK") {# If user clicked "OK"
+if ($msgbox -eq "OK") {# If user clicked "OK"
     # User accepted
 } else {
     # User declined, canceling the script
@@ -32,29 +30,31 @@ if ($result -eq "OK") {# If user clicked "OK"
 
 #-------------------------------------------- Progress-bar definition ---------------------------------------------
 
-#$TotalSteps = 100 
+$TotalSteps = 4 
 
-#function Show-CustomProgressBar {
-#    param (
-#        [int]$CurrentStep,
-#        [int]$TotalSteps
-#    )
+function Show-CustomProgressBar {
+    param (
+        [int]$CurrentStep,
+        [int]$TotalSteps
+    )
     
-#    $ProgressWidth = 50 
-#    $ProgressBar = [string]::Join('', ('|' * [math]::Round(($CurrentStep / $TotalSteps) * $ProgressWidth)))
+    $ProgressWidth = 50 
+    $ProgressBar = [string]::Join('', ('|' * [math]::Round(($CurrentStep / $TotalSteps) * $ProgressWidth)))
     
-#    Write-Host -NoNewline "`r[$ProgressBar] $([math]::Round(($CurrentStep / $TotalSteps) * 100))%"
+    Write-Host -NoNewline "`r[$ProgressBar] $([math]::Round(($CurrentStep / $TotalSteps) * 4))/4 $stepName"
 
-#    if ($CurrentStep -eq $TotalSteps) {
-#        Write-Host ""  
-#    }
-#}
+    if ($CurrentStep -eq $TotalSteps) {
+        Write-Host ""  
+    }
+}
 
 #Show-CustomProgressBar -CurrentStep <percentage> -TotalSteps $TotalSteps
 
 #----------------------------------------------- Folder creation ------------------------------------------------
+$stepName = "Creating folder"
+Show-CustomProgressBar -CurrentStep 1 -TotalSteps $TotalSteps
 
-$folderName = "Client-apps" #Defining the destination folder name
+$folderName = "apps-list" #Defining the destination folder name
 
 if([System.IO.Directory]::Exists($folderName)) #If the folder exists
 {
@@ -64,23 +64,12 @@ else{ #Else
     New-Item $folderName -ItemType Directory | Out-Null #Creating the folder silently
 }
 
-#-------------------------------------------- Defining file names ----------------------------------------------
-
-$userName = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.Split('\')[-1]
- 
-$fileName = "results.csv"
-$fileName2 = "Applications-" + $userName + ".csv"
-
-#-------------------------------------------- Full installed software list ---------------------------------------------
-
-$appsList = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
-                  Select-Object DisplayName, DisplayVersion, Publisher |
-                  Where-Object { $_.DisplayName -ne $null } |
-                  Sort-Object DisplayName
-
 #---------------------------------------------- Defining objects -----------------------------------------------
 
-$systemInfo = Get-CimInstance Win32_ComputerSystem | Select-Object Manufacturer, Model, TotalPhysicalMemory, Domain # Obtain PC specs
+$stepName = "Defining objects"
+Show-CustomProgressBar -CurrentStep 2 -TotalSteps $TotalSteps
+
+$systemInfo = Get-CimInstance Win32_ComputerSystem | Select-Object Manufacturer, Model, TotalPhysicalMemory, Domain, UserName # Obtain PC specs
 
 $biosInfo = Get-CimInstance Win32_BIOS | Select-Object SerialNumber, SMBIOSBIOSVersion # Obtain the computer S/N and BIOS version
 
@@ -88,7 +77,9 @@ $processorInfo = Get-CimInstance Win32_Processor | Select-Object Name, MaxClockS
 
 $gpuInfo = Get-CimInstance Win32_VideoController | Where-Object { $_.Name -and $_.DriverVersion -and $_.DriverDate} # Obtain GPU info
  
-$diskInfo = Get-PhysicalDisk | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB'} #Get the disk type and his RPM (if it's HDD)
+$physicalDisksInfo = Get-PhysicalDisk | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB'} #Get the disk type and his RPM (if it's HDD)
+
+$diskInfo = Get-Disk | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB'} #Get the disk informations(for health status)
 
 $totalSpace = Get-Volume | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB'}  # Get the total volume ingoring USB, Removable and CD-ROM devices
 
@@ -98,41 +89,24 @@ $encryptionStatus = manage-bde -status C: | Out-String #Check if your computer i
 
 $currentDate = Get-Date -Format "yyyy-MM-dd" # Obtain the date
 
-#----------------------------------------------- Finding office licence ------------------------------------------------
+$initialInstallDate = Get-ItemProperty 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\' | Select-Object -ExpandProperty InstallDate # Obtain the initial OS install date in registery key
 
-$365 = $appsList | Where-Object { $_.DisplayName -like "*365*"} # $365 take the name of 365 software
-$office = $appsList | Where-Object {$_.DisplayName -like "*Office*"} # $office take the name of Office software
+$appsList = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* # Registery key
 
-if ($365) { 
-    $365 =  if ($365 -match '=([^;]+)') { $matches[1] } else { $null } # Cut the text to only get the software version
-    $officeVersion = $365
-} if ($office) {
-    $office = if($office -match '=([^;]+)') {$matches[1]} else {$null} # Cut the text to only get the software version
-    $officeVersion = $office
-} else {
-    Write-Host "No Office version found."
-}
-
-#-------------------------------------------- Searching a specific software ---------------------------------------------
-
-#$specificSoftware = "Your Sofware Name" # Define the software to research
-#$softwareVersion = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*, # Checking the \Uninstall folder
-#                                   HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
-#                   Where-Object { $_.DisplayName -like "*$specificSoftware*" } | # Searching for your software
-#                   Select-Object -ExpandProperty DisplayVersion -First 1 # Get the sofware version
-
- 
 #------------------------------------------------- Creating global tab --------------------------------------------------
 
+$stepName = "Generating tab"
+Show-CustomProgressBar -CurrentStep 3 -TotalSteps $TotalSteps
+
 $combinedData = [PSCustomObject]@{
-    "Username" = $userName
+    "Username" = $systemInfo.UserName.Split('\')[-1]
     "Model" = $systemInfo.Model
     "Manufacturer" = $systemInfo.Manufacturer
     "S/N" = $biosInfo.SerialNumber
     "BIOS Version" = $biosInfo.SMBIOSBIOSVersion
     "Computer name" = $osInfo.CSName
     "CPU" = $processorInfo.Name
-    "NUmber of cores" = $processorInfo.NumberOfCores
+    "Number of cores" = $processorInfo.NumberOfCores
     "Frequency" = ($processorInfo.MaxClockSpeed /1000).ToString() + " GHz"
     "GPU" = ($gpuInfo | ForEach-Object {$_.Name}) -join ', '
     "GPU driver version" = ($gpuInfo | ForEach-Object {$_.DriverVersion}) -join ', '
@@ -140,19 +114,27 @@ $combinedData = [PSCustomObject]@{
     "RAM" = [math]::Ceiling([math]::Round($systemInfo.TotalPhysicalMemory / 1GB, 2)).ToString() + " GB"
     "Total disk space" = [math]::Round(($totalSpace | Measure-Object -Property Size -Sum).Sum / 1GB, 2).ToString() + " GB"
     "Total free space" = [math]::Round(($totalSpace | Measure-Object -Property SizeRemaining -Sum).Sum / 1GB, 2).ToString() + " GB" 
-    "Disks type" = ($diskInfo | ForEach-Object { $_.MediaType}) -join ', '
-    "Disks model" = ($diskInfo | ForEach-Object { $_.Model}) -join ', '
+    "Disks type" = ($physicalDisksInfo | ForEach-Object { $_.MediaType}) -join ', '
+    "Disks model" = ($physicalDisksInfo | ForEach-Object { $_.Model}) -join ', '
+    "Disks health" = ($diskInfo| ForEach-Object { $_.HealthStatus}) -join ', '
+    "Disks partitions" = ($diskInfo| ForEach-Object { $_.PartitionStyle}) -join ', '
     "OS" = $osInfo.Caption
     "Version" = $osInfo.Version
     "Architecture" = $osInfo.OSArchitecture
     "Domain" = $systemInfo.Domain
-    "Office version" = $officeVersion
-#    "Specific software" = $softwareVersion
+#   "Specific software" = $appsList | Where-Object { $_.DisplayName -like "* YourSoftwareName *" } | Select-Object -ExpandProperty DisplayVersion -First 1 # Searching for a specific software version
     "BitLocker encryption" = if ($encryptionStatus -match "Protection On") { "Yes" } else { "No" }
+    "Initial install date" = ((Get-Date 01.01.1970)+([System.TimeSpan]::fromseconds($initialInstallDate))).ToString("yyyy-MM-dd")
     "Scan date" = $currentDate
 }
 
 #----------------------------------------------- Exporting all CSV files ------------------------------------------------
 
+$stepName = "Exporting files"
+Show-CustomProgressBar -CurrentStep 4 -TotalSteps $TotalSteps
+
+$fileName = "results.csv"
+$fileName2 = $systemInfo.UserName.Split('\')[-1] + "-" + $osInfo.CSName + ".csv"
+
 $combinedData | Export-Csv -Path $fileName -Delimiter ";" -Append -NoTypeInformation
-$appsList | Export-Csv -Path "$folderName\$fileName2" -Delimiter ";" -Append -NoTypeInformation
+$appsList | Select-Object DisplayName, DisplayVersion, Publisher | Where-Object { $_.DisplayName -ne $null } | Sort-Object DisplayName | Export-Csv -Path "$folderName\$fileName2" -Delimiter ";" -Append -NoTypeInformation
