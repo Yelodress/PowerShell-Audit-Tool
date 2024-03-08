ebdbe5a08a6ab4fa740959536e495447e2cb18ad
Yelodress <82002356+Yelodress@users.noreply.github.com>
2024-03-08 23:57:48 +0100

    v0.5

---
+++ documentation.md
@@ -0,0 +1,77 @@
+# Documentation
+
+### Please note that this script is modular. Some parameters are in comments. Uncomment them to use them.
+
+## UFT-8 conversion
+If you're facing an issue with special characters in your custom text, consider converting them to UTF-8:
+```PowerShell
+$yourVarName = [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::Default.GetBytes("Your text"))
+```
+
+## Adding a step to the progress-bar
+### Adding a step:
+```PowerShell
+$stepName = "Your Step"
+Show-CustomProgressBar -CurrentStep X -TotalSteps $TotalSteps
+```
+(do not forget to replace X by your step number)
+
+### Progress-bar settings to modify
+Replace every X by your total amount of step
+```PowerShell
+$TotalSteps = X 
+
+function Show-CustomProgressBar {
+    param (
+        [int]$CurrentStep,
+        [int]$TotalSteps
+    )
+    
+    $ProgressWidth = 50 
+    $ProgressBar = [string]::Join('', ('o' * [math]::Round(($CurrentStep / $TotalSteps) * $ProgressWidth)))
+    
+    Write-Host -NoNewline "`r[$ProgressBar] $([math]::Round(($CurrentStep / $TotalSteps) * X))/X $stepName"
+
+    if ($CurrentStep -eq $TotalSteps) {
+        Write-Host ""  
+    }
+}
+```
+
+## Searching for a specific user/group
+### Specific useror group
+Add this as a new column in `$combinedData`:
+```PowerShell
+"Specific user" = if (Get-LocalUser -Name "Your username") {"Yes"} else {"No"}
+```
+```PowerShell
+"Specific group" = if (Get-LocalGroup -Name "Your group name") {"Yes"} else {"No"}
+```
+
+## Finding a specific software
+Add this as a new column in `$combinedData`
+```PowerShell
+"Specific software" = $appsList | Where-Object { $_.DisplayName -like "* Your software name *" } | Select-Object -ExpandProperty DisplayVersion -First 1
+```
+**Note:** You can remove the asterisks if you're sure of your exact app name.
+
+## Warning messagebox
+You can add a messagebox to warn users from what informations you're going to retrieve.
+Add this on the top of your script:
+```PowerShell
+Add-Type -AssemblyName PresentationFramework # load the assembly .NET framework (to make the script able to create a message box interface)
+$caption = "Title"
+$messageBoxText = "Description"
+$icon = [System.Windows.MessageBoxImage]::Information
+$button = [System.Windows.MessageBoxButton]::OKCancel
+
+$msgbox = [System.Windows.MessageBox]::Show($messageBoxText, $caption, $button, $icon) # Mixing all components
+
+if ($msgbox -eq "OK") {
+    # User accepted, coninue
+}
+else {
+    # User declined, canceling the script
+    exit
+}
+```
--- script.ps1
+++ script.ps1
@@ -3,34 +3,14 @@
 |  _ \ _____      _____ _ __ / \  _   _  __| (_) |_ 
 | |_) / _ \ \ /\ / / _ \ '__/ _ \| | | |/ _` | | __|
 |  __/ (_) \ V  V /  __/ | / ___ \ |_| | (_| | | |_ 
-|_|   \___/ \_/\_/ \___|_|/_/   \_\__,_|\__,_|_|\__|v0.4.1
+|_|   \___/ \_/\_/ \___|_|/_/   \_\__,_|\__,_|_|\__|v0.5
 
 
 "@
- 
-
-#-------------------------------------------- Warning messagebox ---------------------------------------------
-
-Add-Type -AssemblyName PresentationFramework # load the assemply .NET framework (to make the script able to create a message box interface)
-$caption = "Warning"
-$messageBoxText = "This script will collect some hardware and software information (such as your components, your disk space and your OS version).`nIf you don't want this, you can cancel the execution.`nExecute anyway ?"
-$caption = [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::Default.GetBytes($caption))
-$text = [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::Default.GetBytes($messageBoxText))
-$icon = [System.Windows.MessageBoxImage]::Information
-$button = [System.Windows.MessageBoxButton]::OKCancel
-
-$msgbox = [System.Windows.MessageBox]::Show($text, $caption, $button, $icon) # Mixing all components
-
-if ($msgbox -eq "OK") {# If user clicked "OK"
-    # User accepted
-} else {
-    # User declined, canceling the script
-    exit
-}
 
 #-------------------------------------------- Progress-bar definition ---------------------------------------------
 
-$TotalSteps = 4 
+$TotalSteps = 6 
 
 function Show-CustomProgressBar {
     param (
@@ -39,16 +19,14 @@ function Show-CustomProgressBar {
     )
     
     $ProgressWidth = 50 
-    $ProgressBar = [string]::Join('', ('|' * [math]::Round(($CurrentStep / $TotalSteps) * $ProgressWidth)))
+    $ProgressBar = [string]::Join('', ('o' * [math]::Round(($CurrentStep / $TotalSteps) * $ProgressWidth)))
     
-    Write-Host -NoNewline "`r[$ProgressBar] $([math]::Round(($CurrentStep / $TotalSteps) * 4))/4 $stepName"
+    Write-Host -NoNewline "`r[$ProgressBar] $([math]::Round(($CurrentStep / $TotalSteps) * 6))/6 $stepName"
 
     if ($CurrentStep -eq $TotalSteps) {
         Write-Host ""  
     }
 }
-
-#Show-CustomProgressBar -CurrentStep <percentage> -TotalSteps $TotalSteps
 
 #----------------------------------------------- Folder creation ------------------------------------------------
 $stepName = "Creating folder"
@@ -56,11 +34,11 @@ Show-CustomProgressBar -CurrentStep 1 -TotalSteps $TotalSteps
 
 $folderName = "apps-list" #Defining the destination folder name
 
-if([System.IO.Directory]::Exists($folderName)) #If the folder exists
-{
- #Folder exists :shocked_face:
+if ([System.IO.Directory]::Exists($folderName)) { #If the folder exists
+    #Folder exists :shocked_face:
 }
-else{ #Else
+else {
+    #Else
     New-Item $folderName -ItemType Directory | Out-Null #Creating the folder silently
 }
 
@@ -75,15 +53,17 @@ $biosInfo = Get-CimInstance Win32_BIOS | Select-Object SerialNumber, SMBIOSBIOSV
 
 $processorInfo = Get-CimInstance Win32_Processor | Select-Object Name, MaxClockSpeed, NumberOfCores # Obtain CPU name, max clock speed, Number of cores
 
-$gpuInfo = Get-CimInstance Win32_VideoController | Where-Object { $_.Name -and $_.DriverVersion -and $_.DriverDate} # Obtain GPU info
+$gpuInfo = Get-CimInstance Win32_VideoController | Where-Object { $_.Name -and $_.DriverVersion -and $_.DriverDate } # Obtain GPU info
  
-$physicalDisksInfo = Get-PhysicalDisk | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB'} #Get the disk type and his RPM (if it's HDD)
+$physicalDisksInfo = Get-PhysicalDisk | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB' } #Get the disk type and his RPM (if it's HDD)
 
-$diskInfo = Get-Disk | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB'} #Get the disk informations(for health status)
+$diskInfo = Get-Disk | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB' } #Get the disk informations(for health status)
 
-$totalSpace = Get-Volume | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB'}  # Get the total volume ingoring USB, Removable and CD-ROM devices
+$totalSpace = Get-Volume | Where-Object { $_.DriveType -ne 'Removable' -and $_.DriveType -ne 'CD-ROM' -and $_.BusType -ne 'USB' }  # Get the total volume ingoring USB, Removable and CD-ROM devices
 
 $osInfo = Get-CimInstance Win32_OperatingSystem | Select-Object Caption, Version, OSArchitecture, CSName # Obtain OS informations
+
+$networkConf = Get-CimInstance Win32_NetworkAdapterConfiguration -Filter "IPEnabled = TRUE" # Obtain infos about network cards that have an IP address
 
 $encryptionStatus = manage-bde -status C: | Out-String #Check if your computer is encrypted by BitLocker
 
@@ -93,64 +73,71 @@ $initialInstallDate = Get-ItemProperty 'HKLM:\Software\Microsoft\Windows NT\Curr
 
 $appsList = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* # Registery key
 
+$scanID = [Guid]::NewGuid().ToString("N").Substring(0, 8)  # Taking first 8 characters
+
 #---------------------------------------------- Searching for Office app -----------------------------------------------
 
-$365 = $appsList | Where-Object { $_.DisplayName -like "*365*"} | Select-Object -ExpandProperty DisplayName # $365 take the app name found
-$365 = $365 -replace '^Microsoft ', '' -replace ' -.*$', ''
-$office = $appsList | Where-Object {$_.DisplayName -like "*Microsoft Office*" -and $_.Displayname -notlike "*Microsoft Teams*"} | Select-Object -ExpandProperty DisplayName # $office take the app name found
-$office = $office -replace '^Microsoft ', '' -replace ' -.*$', ''
-
-if ($365) { # If $365 contain something
-    $officeVersion = $365
-} elseif ($office) { # If $office contain something
-    $officeVersion = $office
-} else {
-    Write-Host "No office app found."
-}
-
-#------------------------------------------------- Creating global tab --------------------------------------------------
-
-$stepName = "Generating tab"
+$stepName = "Searching for Office"
 Show-CustomProgressBar -CurrentStep 3 -TotalSteps $TotalSteps
 
+$office = ($appsList | Where-Object { ($_.DisplayName -like "*365*" -or $_.DisplayName -like "*Microsoft Office*") -and $_.Displayname -notlike "*Microsoft Teams*" } | Select-Object -ExpandProperty DisplayName) -replace '^Microsoft ', '' -replace ' -.*$', '' # $office take the app name found 
+
+#---------------------------------------------- Checking if current user is admin -----------------------------------------------
+$stepName = "Checking your role"
+Show-CustomProgressBar -CurrentStep 4 -TotalSteps $TotalSteps
+
+$administratorsGroupName = (New-Object Security.Principal.SecurityIdentifier("S-1-5-32-544")).Translate([Security.Principal.NTAccount]).Value # Identifying Administrator group by his SID (to prevent langage change)
+$administratorsGroupName = $administratorsGroupName.Split('\')[-1] # Cut it before the backslash
+$adminGroupMembers = net localgroup "$administratorsGroupName" # Checking all usernames in this admin group
+$userName = $systemInfo.UserName.Split('\')[-1] # Cutting current username before the backslash
+
+#------------------------------------------------- Creating global tab --------------------------------------------------
+$stepName = "Generating tab"
+Show-CustomProgressBar -CurrentStep 5 -TotalSteps $TotalSteps
+
 $combinedData = [PSCustomObject]@{
-    "Username" = $systemInfo.UserName.Split('\')[-1]
-    "Model" = $systemInfo.Model
-    "Manufacturer" = $systemInfo.Manufacturer
-    "S/N" = $biosInfo.SerialNumber
-    "BIOS Version" = $biosInfo.SMBIOSBIOSVersion
-    "Computer name" = $osInfo.CSName
-    "CPU" = $processorInfo.Name
-    "Number of cores" = $processorInfo.NumberOfCores
-    "Frequency" = ($processorInfo.MaxClockSpeed /1000).ToString() + " GHz"
-    "GPU" = ($gpuInfo | ForEach-Object {$_.Name}) -join ', '
-    "GPU driver version" = ($gpuInfo | ForEach-Object {$_.DriverVersion}) -join ', '
-    "GPU Driver date" = ($gpuInfo | ForEach-Object {$_.DriverDate.ToShortDateString()}) -join ', '
-    "RAM" = [math]::Ceiling([math]::Round($systemInfo.TotalPhysicalMemory / 1GB, 2)).ToString() + " GB"
-    "Total disk space" = [math]::Round(($totalSpace | Measure-Object -Property Size -Sum).Sum / 1GB, 2).ToString() + " GB"
-    "Total free space" = [math]::Round(($totalSpace | Measure-Object -Property SizeRemaining -Sum).Sum / 1GB, 2).ToString() + " GB" 
-    "Disks type" = ($physicalDisksInfo | ForEach-Object { $_.MediaType}) -join ', '
-    "Disks model" = ($physicalDisksInfo | ForEach-Object { $_.Model}) -join ', '
-    "Disks health" = ($diskInfo| ForEach-Object { $_.HealthStatus}) -join ', '
-    "Disks partitions" = ($diskInfo| ForEach-Object { $_.PartitionStyle}) -join ', '
-    "OS" = $osInfo.Caption
-    "Version" = $osInfo.Version
-    "Architecture" = $osInfo.OSArchitecture
-    "Domain" = $systemInfo.Domain
-    "Office Version" = $officeVersion
-#   "Specific software" = $appsList | Where-Object { $_.DisplayName -like "* YourSoftwareName *" } | Select-Object -ExpandProperty DisplayVersion -First 1 # Searching for a specific software version
+    "Username"             = $userName
+    "Administrator"        = if ($adminGroupMembers -match $userName) { "Yes" } else { "No" }
+    "Model"                = $systemInfo.Model
+    "Manufacturer"         = $systemInfo.Manufacturer
+    "S/N"                  = $biosInfo.SerialNumber
+    "BIOS Version"         = $biosInfo.SMBIOSBIOSVersion
+    "Computer name"        = $osInfo.CSName
+    "CPU"                  = $processorInfo.Name
+    "Number of cores"      = $processorInfo.NumberOfCores
+    "Frequency"            = ($processorInfo.MaxClockSpeed / 1000).ToString() + " GHz"
+    "GPU"                  = ($gpuInfo | ForEach-Object { $_.Name }) -join ', '
+    "GPU driver version"   = ($gpuInfo | ForEach-Object { $_.DriverVersion }) -join ', '
+    "GPU Driver date"      = ($gpuInfo | ForEach-Object { $_.DriverDate.ToShortDateString() }) -join ', '
+    "RAM"                  = [math]::Ceiling([math]::Round($systemInfo.TotalPhysicalMemory / 1GB, 2)).ToString() + " GB"
+    "Total disk space"     = [math]::Round(($totalSpace | Measure-Object -Property Size -Sum).Sum / 1GB, 2).ToString() + " GB"
+    "Total free space"     = [math]::Round(($totalSpace | Measure-Object -Property SizeRemaining -Sum).Sum / 1GB, 2).ToString() + " GB" 
+    "Disks type"           = ($physicalDisksInfo | ForEach-Object { $_.MediaType }) -join ', '
+    "Disks model"          = ($physicalDisksInfo | ForEach-Object { $_.Model }) -join ', '
+    "Disks health"         = ($diskInfo | ForEach-Object { $_.HealthStatus }) -join ', '
+    "Disks partitions"     = ($diskInfo | ForEach-Object { $_.PartitionStyle }) -join ', '
+    "OS"                   = $osInfo.Caption
+    "Version"              = $osInfo.Version
+    "Architecture"         = $osInfo.OSArchitecture
+    "Domain"               = $systemInfo.Domain
+    "IP Address"           = ($networkConf | ForEach-Object { $_.IPAddress }) -join ', '
+    "Gateway"              = ($networkConf | ForEach-Object { $_.DefaultIPGateway }) -join ', '
+    "DNS"                  = ($networkConf | ForEach-Object { $_.DNSServerSearchOrder }) -join ', '
+    "DHCP"                 = ($networkConf | ForEach-Object { if ($_.DHCPEnabled) { "Yes" } else { "No" } }) -join ', '
+    "Office Version"       = if ($office) { $office } else { "No" }
     "BitLocker encryption" = if ($encryptionStatus -match "Protection On") { "Yes" } else { "No" }
-    "Initial install date" = ((Get-Date 01.01.1970)+([System.TimeSpan]::fromseconds($initialInstallDate))).ToString("yyyy-MM-dd")
-    "Scan date" = $currentDate
+    "Initial install date" = ((Get-Date 01.01.1970) + ([System.TimeSpan]::fromseconds($initialInstallDate))).ToString("yyyy-MM-dd")
+    "Scan date"            = $currentDate
+    "Scan ID"              = $scanID
 }
 
 #----------------------------------------------- Exporting all CSV files ------------------------------------------------
 
 $stepName = "Exporting files"
-Show-CustomProgressBar -CurrentStep 4 -TotalSteps $TotalSteps
+Show-CustomProgressBar -CurrentStep 6 -TotalSteps $TotalSteps
 
 $fileName = "results.csv"
-$fileName2 = $systemInfo.UserName.Split('\')[-1] + "-" + $osInfo.CSName + ".csv"
+$fileName2 = $systemInfo.UserName.Split('\')[-1] + "-" + $scanID + ".csv"
 
 $combinedData | Export-Csv -Path $fileName -Delimiter ";" -Append -NoTypeInformation
 $appsList | Select-Object DisplayName, DisplayVersion, Publisher | Where-Object { $_.DisplayName -ne $null } | Sort-Object DisplayName | Export-Csv -Path "$folderName\$fileName2" -Delimiter ";" -Append -NoTypeInformation
